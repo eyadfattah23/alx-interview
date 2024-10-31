465c0b9985c4b296f58e5ba89d58a5a1a571e7f8
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-10-31 18:56:23 +0300

    rewrite the code in a clearer way and test it

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,92 +6,85 @@ from typing import List
 
 
 def to_binary(number: int) -> str:
-    """converts a number to a binary string
+    """converts a decimal integer into a binary string
 
     Args:
-        number (int)
+        number (int): number to convert
 
     Returns:
-        str: 8 bit binary string representation of a number
-        , and 8 lsb if the number is more than 8 bits long
+        str: binary representation of the given number
     """
-    return "{:08b}".format(number) if number < 256 \
-        else "{:08b}".format(number)[-8:]
+    return "{:08b}".format(number)[-8:]
 
 
-def calc_char_len(number: str) -> int:
-    """count the length of a character in bytes in a utf-8 string
+def count_char_bytes(number: str) -> int:
+    """counts the bytes of the utf char in the given binary number
+        where a 1 byte char starts with '0' or '10'
 
     Args:
-        number (str): binary representation of the number to check
+        number (str): binary representation of the char
 
     Returns:
-        int: the length of the utf-8 character
+        int: msb 1's bits or 1 for 1 byte characters
     """
-    # if the byte starts with 0 it's a 1 byte character
+
     if number[0] == '0':
         return 1
 
     result = 0
-    for bit in number:
-        if bit == '0':
+    for i in number:
+        if i == '0':
             break
         result += 1
-
-    # result cannot exceed 4 in utf-8
     return result if result <= 4 else 0
 
 
 def validUTF8(data: List[int]) -> bool:
-    """determines if a given data set represents a valid UTF-8 encoding.
-    by combining the use of
-        the check_next_bytes, to_binary and calc_char_len functions
+    """check the validity of a UTF8 string
 
     Args:
-        data (List[int]): list of int representations of chars in a utf-8
-                        string.
+        data (List[int]): utf8 string representation in numbers list
 
     Returns:
-        bool: True if data is a valid UTF-8 encoding, else return False
+        bool: True if the list is a valid UTF8 string
+            else False
     """
-
-    def check_next_bytes(curr_byte_idx: int, n_bytes_to_check: int) -> bool:
-        """validate the next number of bytes in the utf-8 string
-            according to number of msb bits which equal 1
-                returned by calc_char_len
-
-            Example:
-                the list starts with [111011100]
-                this means the next 2 bytes ('1110'->
-                            {3 1's - current byte count}
-                should start with '10'
-
+    def check_next_bytes(curr_char_idx: int, n_bytes: int) -> bool:
+        """check the validity of the next number of bytes of the
+            utf8 char known by it's index supposing it's not a 1 byte char.
 
         Args:
-            curr_byte_idx (int): _description_
-            number_of_bytes_to_check (int): _description_
+            curr_char_idx (int): idx of the char int he utf8 numbers/chars list
+            n_bytes (int): number of bytes returned by count_char_bytes
+                that defines the length of this char
 
+        Returns:
+            bool: True if the char in data[curr_char_idx] is a valid utf8
+                otherwise false
         """
-        for i in range(curr_byte_idx + 1, curr_byte_idx + n_bytes_to_check):
+
+        for i in range(curr_char_idx + 1, curr_char_idx + n_bytes):
             if i >= len(data) or to_binary(data[i])[:2] != '10':
                 return False
         return True
 
-    utf8_char_idx = 0
-    while utf8_char_idx < len(data):
-        char_bytes = calc_char_len(to_binary(data[utf8_char_idx]))
-        first_byte = to_binary(data[utf8_char_idx])
+    utf8_byte_idx = 0
+    while utf8_byte_idx < len(data):
+        char_bytes = count_char_bytes(to_binary(data[utf8_byte_idx]))
 
         if char_bytes > 4 or char_bytes == 0:
             return False
 
-        if char_bytes == 1:
-            if first_byte[0] != '0':
-                return False
-            else:
-                if not check_next_bytes(utf8_char_idx, char_bytes):
-                    return False
+        if char_bytes > 1 and not check_next_bytes(utf8_byte_idx, char_bytes):
+            return False
 
-        utf8_char_idx += char_bytes
+        elif char_bytes == 1:
+            if to_binary(data[utf8_byte_idx])[0] == '0':
+                utf8_byte_idx += char_bytes
+                continue
+            else:
+                return False
+
+        utf8_byte_idx += char_bytes
 
     return True
