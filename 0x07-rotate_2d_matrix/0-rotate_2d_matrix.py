7d375d995fbdc7b25916f1c34406b1e234d8112b
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-11-21 23:37:33 +0200

    add main file fro testing and remove testing lines from the script

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -30,12 +30,3 @@ def rotate_2d_matrix(matrix):
             matrix[n-1-i][j] = matrix[n-1-j][n-1-i]
             matrix[n-1-j][n-1-i] = matrix[i][n-1-j]
             matrix[i][n-1-j] = temp
-
-
-matrix = [[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]]
-
-
-rotate_2d_matrix(matrix)
-
-for row in matrix:
-    print(row)
